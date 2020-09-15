fb706b5f58b4aa05489c3df3ce7f05e1bc181cae
Alvaro <alvaro0308@gmail.com>
2020-09-16 01:20:59 +0200

    Titles and labels

--- main.py
+++ main.py
@@ -15,42 +15,18 @@ def printPoints(sheet, firstRow, firstCol, lastCol, maxClubs, gamesPlayed):
             plot(points)
 
 def plot(points):
-    #points = [0, -1, 0.35, 0.5, 0.75, 1.5, -1, -1.5, 0, 0.25]
+    points = [3.0, 0, 0.25, 0.5, 0, 0.25, 3.0, 1.0, 2.5, 2, 3.0, 0, 0, 0.75, 3.0, 1.0, 0.25, 0.5, 0, 0.25, 3.0, 1.0, 3.0, 0, 0, 0.75, 3.0, 1.0, 0.25, 3.0, 1.0, 0.5, 2.5, 0.5, 0, 0, 0.75, 3.0]
     range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38]
-    fig = plt.figure(num = None, figsize = (80, 3), dpi=80, facecolor = 'w', edgecolor = 'k')
-    ax = fig.add_axes([0.0, 0.0, 1, 1])
+    #fig = plt.figure(num = None, figsize = (80, 3), dpi=80, facecolor = 'w', edgecolor = 'k')
+    fig, ax = plt.subplots()
+    fig.set_size_inches(10, 2.5)
     ax.scatter(range, points, color = 'b')
-    fig.suptitle('test title')
-    #fig.set_xlabel('categories')
-    plt.xlabel('xlabel')
-    plt.ylabel('ylabel')
-
-    #plt.plot(range, points)
-
+    ax.plot(range, points)
+    ax.set_ylabel('Puntuación', fontweight = 'bold')
+    ax.set_xlabel('Jornadas', fontweight = 'bold')
+    ax.grid(True)
+    ax.set_title('Club\n', fontsize = 14, fontweight = 'bold')
     plt.show()
-
-    # Fake dataset
-    height = [3, 12, 5, 18, 45]
-    bars = ('A', 'B', 'C', 'D', 'E')
-    y_pos = np.arange(len(bars))
-
-    # Create bars and choose color
-    plt.bar(y_pos, height, color = (0.5,0.1,0.5,0.6))
-
-    # Add title and axis names
-    plt.title('My title')
-    plt.xlabel('categories')
-    plt.ylabel('values')
-
-    # Limits for the Y axis
-    plt.ylim(0,60)
-
-    # Create names
-    plt.xticks(y_pos, bars)
-
-    # Show graphic
-    plt.show()
-
 
 def main():
     workbook = load_workbook(filename = "Quiniela.xlsx")
