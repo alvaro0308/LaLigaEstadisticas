aea06df055066bbe0e1cf2207747c5cec32b50a4
Alvaro <alvaro0308@gmail.com>
2020-09-22 11:46:34 +0200

    AppUI.py

---
+++ LaLigaEstadisticas/src/main/python/AppUI.py
@@ -0,0 +1,151 @@
+"""AppUI.py."""
+
+from PyQt5.QtWidgets import QRadioButton
+from PyQt5.QtWidgets import QComboBox
+from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QToolBar
+import sip
+
+
+class AppUI(QMainWindow):
+    """Este es el docstring de la funcion."""
+
+    def __init__(self, listClubsSantander, listClubsSmartbank):
+        """Este es el docstring de la funcion."""
+        super().__init__()
+        self.listSantander = listClubsSantander
+        self.listSmartbank = listClubsSmartbank
+        self.setFixedSize(1000, 1000)
+
+        self.generalLayout = QHBoxLayout()
+        self._centralWidget = QWidget()
+        self.setCentralWidget(self._centralWidget)
+        self._centralWidget.setLayout(self.generalLayout)
+
+        toolbar = QToolBar("Main")
+        self.addToolBar(toolbar)
+        self.currencyList = QComboBox()
+
+        self._createBox()
+        self._connectSignals()
+        self._check = True
+        self._radioButtonState = False
+        self._radioButton2State = False
+
+    def _createBox(self):
+        """Este es el docstring de la funcion."""
+        self._box = QComboBox()
+        self._box.addItems(["Seleccione un campeonato",
+                            "Liga Santander", "Liga Smartbank"])
+        self.setWindowTitle("LaLigaEstadisticas")
+        self.generalLayout.addWidget(self._box)
+
+    def _createRadioButton(self):
+        """Este es el docstring de la funcion."""
+        self.radioButtons = {}
+        radioButtonsLayout = QGridLayout()
+        # Button text | position on the QGridLayout
+        self.radioButtons = {
+            self.listSantander[0]: (0, 0),
+            self.listSantander[1]: (1, 0),
+            self.listSantander[2]: (2, 0),
+            self.listSantander[3]: (3, 0),
+            self.listSantander[4]: (4, 0),
+            self.listSantander[5]: (5, 0),
+            self.listSantander[6]: (6, 0),
+            self.listSantander[7]: (7, 0),
+            self.listSantander[8]: (8, 0),
+            self.listSantander[9]: (9, 0),
+            self.listSantander[10]: (0, 1),
+            self.listSantander[11]: (1, 1),
+            self.listSantander[12]: (2, 1),
+            self.listSantander[13]: (3, 1),
+            self.listSantander[14]: (4, 1),
+            self.listSantander[15]: (5, 1),
+            self.listSantander[16]: (6, 1),
+            self.listSantander[17]: (7, 1),
+            self.listSantander[18]: (8, 1),
+            self.listSantander[19]: (9, 1),
+        }
+        # Create the buttons and add them to the grid layout
+        for btnText, pos in self.radioButtons.items():
+            self.radioButtons[btnText] = QRadioButton(btnText)
+            self.radioButtons[btnText].setFixedSize(130, 20)
+            self.radioButtons[btnText].setChecked(False)
+            radioButtonsLayout.addWidget(self.radioButtons[btnText],
+                                         pos[0], pos[1])
+            # self._radioButton.toggled.connect(lambda:
+            #                                   self.btnstate(self._radioButton))
+        self.generalLayout.addLayout(radioButtonsLayout)
+        self._radioButtonState = True
+
+    def _createRadioButton2(self):
+        """Este es el docstring de la funcion."""
+        self.radioButtons2 = {}
+        radioButtons2Layout = QGridLayout()
+        # Button text | position on the QGridLayout
+        self.radioButtons2 = {
+            self.listSmartbank[0]: (0, 0),
+            self.listSmartbank[1]: (1, 0),
+            self.listSmartbank[2]: (2, 0),
+            self.listSmartbank[3]: (3, 0),
+            self.listSmartbank[4]: (4, 0),
+            self.listSmartbank[5]: (5, 0),
+            self.listSmartbank[6]: (6, 0),
+            self.listSmartbank[7]: (7, 0),
+            self.listSmartbank[8]: (8, 0),
+            self.listSmartbank[9]: (9, 0),
+            self.listSmartbank[10]: (0, 1),
+            self.listSmartbank[11]: (1, 1),
+            self.listSmartbank[12]: (2, 1),
+            self.listSmartbank[13]: (3, 1),
+            self.listSmartbank[14]: (4, 1),
+            self.listSmartbank[15]: (5, 1),
+            self.listSmartbank[16]: (6, 1),
+            self.listSmartbank[17]: (7, 1),
+            self.listSmartbank[18]: (8, 1),
+            self.listSmartbank[19]: (9, 1),
+        }
+        # Create the buttons and add them to the grid layout
+        for btnText, pos in self.radioButtons2.items():
+            self.radioButtons2[btnText] = QRadioButton(btnText)
+            self.radioButtons2[btnText].setFixedSize(120, 20)
+            self.radioButtons2[btnText].setChecked(False)
+            radioButtons2Layout.addWidget(self.radioButtons2[btnText],
+                                          pos[0], pos[1])
+            # self._radioButton.toggled.connect(lambda:
+            #                                   self.btnstate(self._radioButton))
+        self.generalLayout.addLayout(radioButtons2Layout)
+        self._radioButton2State = True
+
+    def _clearRadioButtons(self):
+        if self._radioButtonState:
+            for btnText, pos in self.radioButtons.items():
+                sip.delete(self.radioButtons[btnText])
+            self._radioButtonState = False
+        if self._radioButton2State:
+            for btnText, pos in self.radioButtons2.items():
+                sip.delete(self.radioButtons2[btnText])
+            self._radioButton2State = False
+
+    def _connectSignals(self):
+        """Este es el docstring de la funcion."""
+        self._box.currentIndexChanged.connect(self._printBox)
+
+    def _printBox(self):
+        """Este es el docstring de la funcion."""
+        if self._box.currentText() == "Seleccione un campeonato":
+            self._clearRadioButtons()
+        else:
+            print("Campeonato seleccionado: ", self._box.currentText())
+            if self._check:
+                # self._check = False
+                if self._box.currentText() == "Liga Santander":
+                    self._clearRadioButtons()
+                    self._createRadioButton()
+                elif self._box.currentText() == "Liga Smartbank":
+                    self._clearRadioButtons()
+                    self._createRadioButton2()
--- LaLigaEstadisticas/src/main/python/main.py
+++ LaLigaEstadisticas/src/main/python/main.py
@@ -1,19 +1,14 @@
 """LaLigaEstadisticas App."""
 
 # Filename: AppUI.py
+# export PYTHONIOENCODING=utf-8
 
-import sys
-import sip
-from openpyxl import load_workbook
-import matplotlib.pyplot as plt
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QGridLayout
-from PyQt5.QtWidgets import QWidget
-from PyQt5.QtWidgets import QHBoxLayout
-from PyQt5.QtWidgets import QComboBox
-from PyQt5.QtWidgets import QRadioButton
+from AppUI import AppUI
+import matplotlib.pyplot as plt
+from openpyxl import load_workbook
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
 
 __version__ = "alpha"
 __author__ = "Álvaro"
@@ -57,6 +52,15 @@ def createLists(points, pointsNew, rangePoints, gamesPlayed):
         rangePoints[j - 1] = j
 
 
+def readClubs(listClubs, maxClubs, firstRow, sheet):
+    """."""
+    for cell in sheet.iter_rows(min_row=firstRow,
+                                max_row=maxClubs + firstRow - 1,
+                                min_col=5, max_col=5,
+                                values_only=True):
+        listClubs.append(cell[0])
+
+
 def createGraphic(pointsNew, rangePoints, gamesPlayed):
     """Create graphic."""
     plt.rcParams['toolbar'] = 'None'
@@ -77,8 +81,8 @@ def createGraphic(pointsNew, rangePoints, gamesPlayed):
 
 def main():
     """Is main function."""
-    workbook = load_workbook(filename="Quiniela.xlsx")
-    workbook.sheetnames
+    workbook = load_workbook(
+        filename="/home/alvaro/github/LaLigaEstadisticas/Quiniela.xlsx")
     sheet = workbook.active
 
     maxClubsSantander = 20
@@ -90,165 +94,28 @@ def main():
 
     gamesPlayed = 38
 
-    print("Liga Santander")
-    printPoints(sheet, firstRowSantander, firstCol,
-                lastCol, maxClubsSantander, gamesPlayed)
-    print("\nLiga Smartbank")
-    printPoints(sheet, firstRowSmartbank, firstCol,
-                lastCol, maxClubsSmartbank, gamesPlayed)
+    listClubsSantander = []
+    listClubsSmartbank = []
+    readClubs(listClubsSantander, maxClubsSantander,
+              firstRowSantander, sheet)
+    readClubs(listClubsSmartbank, maxClubsSmartbank,
+              firstRowSmartbank, sheet)
 
+    # print("Liga Santander")
+    # printPoints(sheet, firstRowSantander, firstCol,
+    #             lastCol, maxClubsSantander, gamesPlayed)
+    # print("\nLiga Smartbank")
+    # printPoints(sheet, firstRowSmartbank, firstCol,
+    #             lastCol, maxClubsSmartbank, gamesPlayed)
 
-class AppUI(QMainWindow):
-    """Este es el docstring de la funcion."""
-
-    def __init__(self):
-        """Este es el docstring de la funcion."""
-        super().__init__()
-        self.setFixedSize(1000, 1000)
-        self.generalLayout = QHBoxLayout()
-        self._centralWidget = QWidget(self)
-        self.setCentralWidget(self._centralWidget)
-        self._centralWidget.setLayout(self.generalLayout)
-        self._createBox()
-        self._connectSignals()
-        self._check = True
-        self._radioButtonState = False
-        self._radioButton2State = False
-
-    def _createBox(self):
-        """Este es el docstring de la funcion."""
-        self._box = QComboBox()
-        self._box.addItems(["Seleccione un campeonato",
-                            "Liga Santander", "Liga Smartbank"])
-        self.setWindowTitle("LaLigaEstadisticas")
-        self.generalLayout.addWidget(self._box)
-
-    def _createRadioButton(self):
-        """Este es el docstring de la funcion."""
-        self.radioButtons = {}
-        radioButtonsLayout = QGridLayout()
-        # Button text | position on the QGridLayout
-        self.radioButtons = {
-            "1": (0, 0),
-            "2": (0, 1),
-            "3": (1, 0),
-            "4": (1, 1),
-            "5": (2, 0),
-            "6": (2, 1),
-            "7": (3, 0),
-            "8": (3, 1),
-            "9": (4, 0),
-            "10": (4, 1),
-            "11": (5, 0),
-            "12": (5, 1),
-            "13": (6, 0),
-            "14": (6, 1),
-            "15": (7, 0),
-            "16": (7, 1),
-            "17": (8, 0),
-            "18": (8, 1),
-            "19": (9, 0),
-            "20": (9, 1),
-        }
-        # Create the buttons and add them to the grid layout
-        for btnText, pos in self.radioButtons.items():
-            self.radioButtons[btnText] = QRadioButton(btnText)
-            self.radioButtons[btnText].setFixedSize(120, 20)
-            self.radioButtons[btnText].setChecked(False)
-            radioButtonsLayout.addWidget(self.radioButtons[btnText],
-                                         pos[0], pos[1])
-            # self._radioButton.toggled.connect(lambda:
-            #                                   self.btnstate(self._radioButton))
-        self.generalLayout.addLayout(radioButtonsLayout)
-        self._radioButtonState = True
-
-    def _createRadioButton2(self):
-        """Este es el docstring de la funcion."""
-        self.radioButtons2 = {}
-        radioButtons2Layout = QGridLayout()
-        # Button text | position on the QGridLayout
-        self.radioButtons2 = {
-            "a": (0, 0),
-            "b": (0, 1),
-            "c": (1, 0),
-            "d": (1, 1),
-            "e": (2, 0),
-            "f": (2, 1),
-            "g": (3, 0),
-            "h": (3, 1),
-            "i": (4, 0),
-            "j": (4, 1),
-            "k": (5, 0),
-            "l": (5, 1),
-            "m": (6, 0),
-            "n": (6, 1),
-            "o": (7, 0),
-            "p": (7, 1),
-            "q": (8, 0),
-            "r": (8, 1),
-            "s": (9, 0),
-            "t": (9, 1),
-            "u": (10, 0),
-            "v": (10, 1),
-        }
-        # Create the buttons and add them to the grid layout
-        for btnText, pos in self.radioButtons2.items():
-            self.radioButtons2[btnText] = QRadioButton(btnText)
-            self.radioButtons2[btnText].setFixedSize(120, 20)
-            self.radioButtons2[btnText].setChecked(False)
-            radioButtons2Layout.addWidget(self.radioButtons2[btnText],
-                                          pos[0], pos[1])
-            # self._radioButton.toggled.connect(lambda:
-            #                                   self.btnstate(self._radioButton))
-        self.generalLayout.addLayout(radioButtons2Layout)
-        self._radioButton2State = True
-
-    def _clearRadioButtons(self):
-        if self._radioButtonState:
-            for btnText, pos in self.radioButtons.items():
-                sip.delete(self.radioButtons[btnText])
-            self._radioButtonState = False
-        if self._radioButton2State:
-            for btnText, pos in self.radioButtons2.items():
-                sip.delete(self.radioButtons2[btnText])
-            self._radioButton2State = False
-
-    def _connectSignals(self):
-        """Este es el docstring de la funcion."""
-        self._box.currentIndexChanged.connect(self._printBox)
-
-    def _printBox(self):
-        """Este es el docstring de la funcion."""
-        if self._box.currentText() == "Seleccione un campeonato":
-            self._clearRadioButtons()
-        else:
-            print("Campeonato seleccionado: ", self._box.currentText())
-            if self._check:
-                # self._check = False
-                if self._box.currentText() == "Liga Santander":
-                    self._clearRadioButtons()
-                    self._createRadioButton()
-                elif self._box.currentText() == "Liga Smartbank":
-                    self._clearRadioButtons()
-                    self._createRadioButton2()
-
-
-def main2():
-    """Second main funtion."""
-    # Create an instance of `QApplication`
-    LaLiga = QApplication(sys.argv)
-    # Show the calculator's GUI
-    view = AppUI()
+    app = QApplication(sys.argv)
+    view = AppUI(listClubsSantander, listClubsSmartbank)
     view.show()
-    # Create instances of the model and the controller
-    # model = evaluateExpression
-    # AppCtrl(view = view)
-    # Execute calculator's main loop
-    sys.exit(LaLiga.exec_())
+    sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
-    main2()
+    main()
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
     window = QMainWindow()
     window.resize(250, 150)
